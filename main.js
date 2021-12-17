01f8f7427196adb9cd67593ba070dc9cd2855166
B-SeungJIn <byeon1031@naver.com>
2021-12-18 00:09:37 +0900

    third commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const subscribe = document.querySelector('.channel .subscribe');
+
+subscribe.addEventListener('click', function(){
+    alert('구독 완료')
+})
+
+// 자바스크립트의 문법을 제대로 공부하고, jQuery에 대해서도 알아봐야겠다.
